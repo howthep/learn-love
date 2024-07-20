ecc917e2e7178bf5425c4a0b2fe8c6407ca3b8e6
howthep <15965394977@163.com>
2024-07-20 22:30:59 +0800

    no need to comment lldebugger

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
+#! /bin/bash
+# perl -ep 's/^require*lldebug)/#\1/' main.lua
+if [ "$1" == 'debug' ]; then
+	echo 'debug'
+	perl -i.bak -pe 's/^--\s*// if /^--.*require.*debug/' main.lua
+else
+	echo 'run'
+	perl -i.bak -pe 's/(.*)/-- \1/ if /^require.*debug/' main.lua
+fi
 cmd='love'
 if ! type $cmd &>/dev/null ;then
 	echo $cmd not found!
